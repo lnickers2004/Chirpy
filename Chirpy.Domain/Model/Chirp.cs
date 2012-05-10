72d5a77baf7fff1ae9b4bca41ba91a275f02a10b
Richard Fawcett <r_d_fawcett@hotmail.com>
2012-05-10 22:09:48 +0100

    Added Html helper RenderChirp This is to replace the ToHtml method of Chirpy.Domain.Model.Chirp.

--- Chirpy.Domain/Model/Chirp.cs
+++ Chirpy.Domain/Model/Chirp.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Net;
 
 namespace Chirpy.Domain.Model
 {
@@ -18,23 +17,6 @@ namespace Chirpy.Domain.Model
         {
             Tags = new List<ChirpTag>();
             User = new ChirpyUser();
-        }
-
-        public string ToHtml()
-        {
-            string result = WebUtility.HtmlEncode(this.Value);
-
-            //TODO Implement better RegEx matching http://stackoverflow.com/a/5367564/431891
-            string[] tokens = this.Value.Split(new char[] { ' ' });
-            for (int tokenCount = 0; tokenCount < tokens.Length; tokenCount++)
-            {
-                if (tokens[tokenCount].StartsWith("#"))
-                {
-                    var encodedToken = WebUtility.HtmlEncode(tokens[tokenCount]);
-                    result = result.Replace(encodedToken, "<a href=\"\">" + encodedToken + "</a>");
-                }
-            }
-            return result;
         }
     }
 }
---
+++ Chirpy.MVC3/Helpers/RenderChirpHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chirpy.Domain.Model;
+using System.Net;
+
+namespace Chirpy.Helpers
+{
+    public static class RenderChirpHelper
+    {
+        public static IHtmlString RenderChirp(this HtmlHelper html, Chirp chirp)
+        {
+            string result = WebUtility.HtmlEncode(chirp.Value);
+
+            //TODO Implement better RegEx matching http://stackoverflow.com/a/5367564/431891
+            string[] tokens = chirp.Value.Split(new char[] { ' ' });
+            for (int tokenCount = 0; tokenCount < tokens.Length; tokenCount++)
+            {
+                if (tokens[tokenCount].StartsWith("#"))
+                {
+                    var encodedToken = WebUtility.HtmlEncode(tokens[tokenCount]);
+                    result = result.Replace(encodedToken, "<a href=\"\">" + encodedToken + "</a>");
+                }
+            }
+            return new MvcHtmlString(result);
+        }
+    }
+}
